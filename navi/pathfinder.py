85cafe63550c4220c411d75acb8c42b0773b401a
Maxime Djmb <maxime.djomby@gmail.com>
2021-10-26 17:12:02 +0200

    Refactoring Added logic for choosing path closest to target

--- navi/pathfinder.py
+++ navi/pathfinder.py
@@ -2,65 +2,111 @@ import math
 import numpy as np
 
 
-def point_coords(distance, inclination, azimuth, sensor_pos):
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+def sphere2cartesian(distance, inclination, azimuth):
     x = distance * math.sin(azimuth) * math.sin(inclination)
     y = distance * math.cos(azimuth) * math.sin(inclination)
     z = distance * math.cos(inclination)
-    return np.array([x, y, z]) + sensor_pos
+    return np.array([x, y, z])
 
 
 def slope(point1, point2):
-    horz = math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
-    vert = point2[2] - point1[2]
-    return vert/horz
+    run = math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+    rise = point2[2] - point1[2]
+    return rise/run
 
 
-def generate_point_cloud(raw_scanline, sensor_pos, sensor_inclination):
-    # TODO: might need to take into account sensor cone_angle
-    point_cloud = []
-    for data_point in raw_scanline:
-        point_cloud.append(point_coords(
-            data_point[1],
-            sensor_inclination,
-            data_point[0],
-            sensor_pos
-        ))
-    return point_cloud
+class Pathfinder:
 
+    def __init__(self, target, sensor_pos, sensor_inclination, min_width, max_angle):
+        self.target = target
 
-def find_traversable(points, max_angle, min_width):
-    # TODO: take into account rover inclination
-    potential_chains = []
-    c = [points[0]]
-    for i in range(1, len(points)-1):
-        # evaluating 'sideways' slope between 2 points
-        # evaluating if terrain is too steep or is a wall by looking at slope between from the rover head to the point
-        slopes = [
-            abs(slope(points[i-1], points[i])),
-            abs(slope(points[i], points[i+1])),
-            abs(slope([0, 0, 0], points[i])),
-        ]
-        if max(slopes) <= math.atan(max_angle):
-            c.append(points[i])
-        else:
-            if len(c) > 1:  # don't want empty lists or single point lists
-                potential_chains.append(c)
-            c = []
-    c.append(points[-1])  # add last point to the chain
-    if len(c) > 1:  # don't want empty lists or single point lists
-        potential_chains.append(c)
-    print('found', len(potential_chains), 'potential chains')
+        # Path picking constraints
+        self.min_width = min_width
+        self.max_angle = max_angle
+        self.sensor_pos = sensor_pos  # sensor position relative to the rover
+        self.sensor_inclination = sensor_inclination   # sensor tilt (on +X) relative to +Z axis
 
-    # culling narrow chains
-    # TODO: rover might need to align itself with normal of traversable zone. Ex: tilted doors
-    chains = []
-    for c in potential_chains:
-        endpoint1, endpoint2 = c[0], c[-1]
-        width = math.sqrt((endpoint2[0]-endpoint1[0])**2 + (endpoint2[1]-endpoint1[1])**2)
-        if width > min_width:
-            print('- width OK:', width)
-            chains.append(c)
-        else:
-            print('- width invalid:', width)
-    print('found', len(chains), 'valid chains')
-    return chains
+        self.forks = []  # list of past encountered forks each fork containing untested paths
+
+    def point_cloud(self, scanline):
+        # TODO: might need to take into account sensor cone_angle
+        point_cloud = []  # list of all scanned points
+        for data_point in scanline:
+            coords = sphere2cartesian(
+                data_point[1],
+                self.sensor_inclination,
+                data_point[0],
+            )
+            point_cloud.append(coords + self.sensor_pos)
+        return point_cloud
+
+    def valid_chains(self, points):
+        # TODO: take into account rover inclination
+        potential_chains = []
+        c = []
+        for i in range(1, len(points) - 1):
+            # evaluating 'sideways' slope between 2 points
+            # evaluating if terrain is too steep or is a wall by looking at slope between the rover head to the point
+            slopes = [
+                abs(slope(points[i - 1], points[i])),
+                abs(slope(points[i], points[i + 1])),
+                abs(slope([0, 0, 0], points[i])),
+            ]
+            if max(slopes) <= math.atan(self.max_angle):
+                c.append(points[i])
+            else:  # traversable chain ended, time to save it
+                if len(c) > 1:  # don't want empty lists or single point lists
+                    potential_chains.append(c)
+                c = []  # empty chain
+        # checking last chain
+        if len(c) > 1:  # don't want empty lists or single point lists
+            potential_chains.append(c)
+
+        # culling narrow chains
+        # TODO: rover might need to align itself with normal of traversable zone. Ex: tilted doors
+        chains = []
+        for c in potential_chains:
+            endpoint1, endpoint2 = c[0], c[-1]
+            width = math.sqrt((endpoint2[0] - endpoint1[0]) ** 2 + (endpoint2[1] - endpoint1[1]) ** 2)
+            if width > self.min_width:
+                chains.append(c)
+
+        # Debug
+        print('found', len(potential_chains), 'potential chains')
+        print('found', len(chains), 'valid chains')
+
+        return chains
+
+    def available_paths(self, chains, rover_pos):
+        target_heading = normalize(self.target - rover_pos)
+        paths = []
+        for c in chains:
+            mid = c[len(c)//2]
+            path_heading = normalize(mid)
+            alignment = np.dot(target_heading, path_heading)
+            paths.append([mid, alignment])
+
+        paths.sort(key=lambda x: x[1], reverse=True)
+
+        # Debug
+        print('All paths at fork:')
+        for p in paths:
+            print('- alignment:', p[1], '\twaypoint:', p[0])
+
+        return paths
+
+    def evaluate_terrain(self, scanline, rover_pos):
+        points = self.point_cloud(scanline)
+        chains = self.valid_chains(points)
+        fork = self.available_paths(chains, rover_pos)
+
+        self.forks.append(fork)
+
+# TODO: Mask points behind walls and fix max obstacle height
--- test/pathfinder_test.py
+++ test/pathfinder_test.py
@@ -8,16 +8,29 @@ from context import navi
 with open("scanline.json", 'r') as f:
     data = json.load(f)
     sensor_pos, sensor_inclination, scanline = data
+max_angle = math.radians(15)
+min_width = 0.5
 
-cloud = np.array(navi.generate_point_cloud(scanline, sensor_pos, sensor_inclination))
+Patterson = navi.Pathfinder(
+    target=np.array([0, 10, 0]),
+    sensor_pos=sensor_pos,
+    sensor_inclination=sensor_inclination,
+    min_width=min_width,
+    max_angle=max_angle
+)
+Patterson.evaluate_terrain(
+    scanline=scanline,
+    rover_pos=np.array([0, 0, 0])
+)
+points = np.array(Patterson.point_cloud(scanline))
+chains = Patterson.valid_chains(points)
 
-chains = navi.find_traversable(cloud, math.radians(15), 0.5)
-
+# Plotting
 fig = plt.figure()
 ax1 = fig.add_subplot(121, projection='3d')
 ax2 = fig.add_subplot(122, projection='3d')
 
-x, y, z = np.around(cloud.T, 2)
+x, y, z = np.around(points.T, 2)
 ax1.plot(x, y, z, 'o-c')
 ax1.set_xlabel('X')
 ax1.set_ylabel('Y')
@@ -26,11 +39,10 @@ ax1.set_zlabel('Z')
 for c in chains:
     x, y, z = np.around(np.array(c).T, 2)
     ax2.plot(x, y, z, '-g')
+    mid_x, mid_y, mid_z = c[len(c)//2]
+    ax2.plot(mid_x, mid_y, mid_z, 'or')
 ax2.set_xlabel('X')
 ax2.set_ylabel('Y')
 ax2.set_zlabel('Z')
-
-
-# TODO: Mask points behind walls and fix max obstacle height
 
 plt.show()
