df7923a0e27d08f46a0027f267c1e115f94ecc20
Nihoncraft <78471570+Nihoncraft@users.noreply.github.com>
2021-12-02 21:34:57 +0100

    Progress on setting interface.

--- Base/interface/main.Designer.cs
+++ Base/interface/main.Designer.cs
@@ -30,9 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rover_Interface));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.Settings_button = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.Settings_button = new System.Windows.Forms.Button();
+            this.Settings_groupbox = new System.Windows.Forms.GroupBox();
+            this.Communication_status = new System.Windows.Forms.Label();
+            this.port_comboBox = new System.Windows.Forms.ComboBox();
+            this.Connectivity_label = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.Settings_groupbox.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -46,20 +51,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(136, 445);
             this.panel1.TabIndex = 0;
-            // 
-            // Settings_button
-            // 
-            this.Settings_button.FlatAppearance.BorderSize = 0;
-            this.Settings_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Settings_button.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Settings_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.Settings_button.Location = new System.Drawing.Point(0, 0);
-            this.Settings_button.Name = "Settings_button";
-            this.Settings_button.Size = new System.Drawing.Size(136, 50);
-            this.Settings_button.TabIndex = 1;
-            this.Settings_button.Text = "Settings";
-            this.Settings_button.UseVisualStyleBackColor = true;
-            this.Settings_button.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -76,12 +67,76 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // Settings_button
+            // 
+            this.Settings_button.FlatAppearance.BorderSize = 0;
+            this.Settings_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Settings_button.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Settings_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.Settings_button.Location = new System.Drawing.Point(0, 0);
+            this.Settings_button.Name = "Settings_button";
+            this.Settings_button.Size = new System.Drawing.Size(136, 50);
+            this.Settings_button.TabIndex = 1;
+            this.Settings_button.Text = "Settings";
+            this.Settings_button.UseVisualStyleBackColor = true;
+            this.Settings_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Settings_groupbox
+            // 
+            this.Settings_groupbox.Controls.Add(this.Communication_status);
+            this.Settings_groupbox.Controls.Add(this.port_comboBox);
+            this.Settings_groupbox.Controls.Add(this.Connectivity_label);
+            this.Settings_groupbox.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Settings_groupbox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.Settings_groupbox.Location = new System.Drawing.Point(142, 12);
+            this.Settings_groupbox.Name = "Settings_groupbox";
+            this.Settings_groupbox.Size = new System.Drawing.Size(324, 420);
+            this.Settings_groupbox.TabIndex = 1;
+            this.Settings_groupbox.TabStop = false;
+            this.Settings_groupbox.Text = "Setttings";
+            // 
+            // Communication_status
+            // 
+            this.Communication_status.AutoSize = true;
+            this.Communication_status.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Communication_status.Location = new System.Drawing.Point(151, 36);
+            this.Communication_status.Name = "Communication_status";
+            this.Communication_status.Size = new System.Drawing.Size(148, 21);
+            this.Communication_status.TabIndex = 2;
+            this.Communication_status.Text = "Searching for Rover";
+            // 
+            // port_comboBox
+            // 
+            this.port_comboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.port_comboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.port_comboBox.FormattingEnabled = true;
+            this.port_comboBox.Items.AddRange(new object[] {
+            "Auto",
+            "None"});
+            this.port_comboBox.Location = new System.Drawing.Point(6, 61);
+            this.port_comboBox.Name = "port_comboBox";
+            this.port_comboBox.Size = new System.Drawing.Size(312, 36);
+            this.port_comboBox.TabIndex = 1;
+            this.port_comboBox.Text = "Port";
+            this.port_comboBox.SelectionChangeCommitted += new System.EventHandler(this.port_comboBox_SelectionChangeCommitted);
+            // 
+            // Connectivity_label
+            // 
+            this.Connectivity_label.AutoSize = true;
+            this.Connectivity_label.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Connectivity_label.Location = new System.Drawing.Point(6, 30);
+            this.Connectivity_label.Name = "Connectivity_label";
+            this.Connectivity_label.Size = new System.Drawing.Size(151, 28);
+            this.Connectivity_label.TabIndex = 0;
+            this.Connectivity_label.Text = "Communication";
+            // 
             // Rover_Interface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(478, 444);
+            this.Controls.Add(this.Settings_groupbox);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -90,6 +145,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Rover Interface";
             this.panel1.ResumeLayout(false);
+            this.Settings_groupbox.ResumeLayout(false);
+            this.Settings_groupbox.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -99,6 +156,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button Settings_button;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox Settings_groupbox;
+        private System.Windows.Forms.Label Connectivity_label;
+        private System.Windows.Forms.ComboBox port_comboBox;
+        private System.Windows.Forms.Label Communication_status;
     }
 }
 
--- Base/interface/main.cs
+++ Base/interface/main.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace @interface
 {
@@ -15,18 +16,39 @@ namespace @interface
         public Rover_Interface()
         {
             InitializeComponent();
+            Settings_groupbox.Hide();
+            Settings_groupbox.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            /*
-             * Open settings. 
-             */
+            Settings_groupbox.Show();
+            Settings_groupbox.Enabled = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void port_comboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            /* Connect with port. */
+        }
+    }
+}
+
+namespace Communication
+{
+    partial class Connectivity
+    {
+        static public void Search()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            for(int i; i < ports.Length; i++)
+            {
+                // Fill port_comboBox with all ports. 
+            }
         }
     }
 }
