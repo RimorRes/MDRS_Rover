5cddf899656c60a19670271e5cccc70cdc7c4c3e
Rodolphe Hoppeler <rodolphe.hoppeler@crans.org>
2022-01-20 23:10:43 +0100

    Update serialComm.cpp
    ça va avec la branche patch-2

--- missionMars2022_progCompletv0/serialComm.cpp
+++ missionMars2022_progCompletv0/serialComm.cpp
@@ -1,66 +1,113 @@
 /*Serial communication between Arduino and Raspberry Pi. */
 #include<Arduino.h>
+#define AFFICHAGE // à commenter pour enlever le bavardage
 
-char* serialPull(int TIMEOUT = 1000){ // Pulls/Reads incoming data from serial port. It does NOT read the instrution. 
-  int incomingByte = 0; // for incoming serial data
-    /*char* RETURN;
-    RETURN[0]=0;
-    Serial.setTimeout(TIMEOUT);
-    for(int i ; Serial.available() > 0 ; i++){ // While the serial port is available. 
-        RETURN[i] = Serial.read(); // Fill RETURN with incoming bytes. 
-    }
-    Serial.println(RETURN);
-    return RETURN;*/
-    // reply only when you receive data:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
-
+String serialPull(int TIMEOUT = 1000){ // Pulls/Reads incoming data from serial port. It does NOT read the instrution. 
+  String phrase;
+  char terminator =';';
+  Serial.setTimeout(TIMEOUT); // le temps au bout duquel on arrête de surveiller le bus
+  phrase = Serial.readStringUntil(terminator);  // on lit le bus jusqu'à trouver le caractère de fin
+  int posFin = phrase.indexOf(char(10));  // on repère le retour à la ligne qui vient lorsqu'on utilise le terminal série
+  if (posFin != -1){phrase.remove(posFin);} // et on l'enlève !
+ 
 #ifdef AFFICHAGE
     // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+    //Serial.println("I received: " + phrase);
 #endif
-  }
+  return phrase;
 }
 
-void serialPush(const char* TXT){ // Send bytes from serial port. Simplification of Serial.Write().
-    Serial.write(TXT, sizeof(TXT));
+void serialPush(String TXT){ // Send bytes from serial port. Simplification of Serial.Write().
+#ifdef AFFICHAGE
+    Serial.println("Coucou, nous sommes dans serialPush().");
+#endif
+    unsigned int longueur = TXT.length();
+    if (longueur != 0){
+      char message[longueur+1]; // +1 car il faut stocker le caractère de fin de chaîne, je crois
+      TXT.toCharArray(message, longueur+1); //  conversion de type
+#ifdef AFFICHAGE
+      Serial.println("   Texte à transmettre : " + TXT + " de longueur " + String(longueur) + ".");
+      Serial.println("   Texte envoyé : " + String(message));
+#endif
+      Serial.write(message, longueur);  // Pas +1, donc sans le caractère de fin.
+#ifdef AFFICHAGE
+      Serial.println(" <-- C'est fait.");
+#endif
+    }
+    return;
 }
 
 void noel(/*const char* ARG*/){
-    digitalWrite(4, HIGH);
-    delay(1000);
-    digitalWrite(4, LOW);
-    delay(1000);
+    digitalWrite(8, HIGH);
+    delay(250);
+    digitalWrite(8, LOW);
+    delay(250);
+    digitalWrite(9, HIGH);
+    delay(250);
+    digitalWrite(9, LOW);
+    delay(250);
     //Serial.println(ARG);
 }
 
-void Run(char* INSTRUCTION){ // Reads the instruction to call it after. 
-    char* FX; FX[0]=0; char* ARG; ARG[0]=0; int INDEX = 0;
-    int instruction_int;
-    for(int i ; i < sizeof(INSTRUCTION) ; i++){
-      instruction_int= (int)INSTRUCTION[i];
-        if(instruction_int != 59){ // 59 is ascii for ";"
-            FX[i] = INSTRUCTION[i];
-        }
-        else{INDEX = i; break;}
-        Serial.print("aléatoire");
-        Serial.println(FX);
-    }
-    for(int i = INDEX; i < sizeof(INSTRUCTION) ; i++){
-        if(INSTRUCTION[i] != 59 && INSTRUCTION[i++] != 59){
-            ARG[i] = INSTRUCTION[i];
-        }else{break;}
-    }
-    switch((int)FX){ // All functions callable from Raspberry Pi shall have a case here. 
-        case 1: 
-            serialPush(ARG);
-        //case "test":
-        //    digitalWrite(4, HIGH);
+void Run(String INSTRUCTION){ // Reads the instruction to call it after. 
+    if (INSTRUCTION != ""){
+#ifdef AFFICHAGE
+      Serial.println("instruction dans Run : " + INSTRUCTION);
+      Serial.println("   taille instruction : " + String(INSTRUCTION.length())); 
+#endif
+      char separator='_'; // caractère de séparation entre fonction et arguments, ou entre arguments
+      
+      // récupération du code de la fonction appelée
+      int posFin = INSTRUCTION.indexOf(separator); // repère le passage de la fonction aux arguments de la fonction
+      int numFonction = INSTRUCTION.substring(0, posFin).toInt(); // lit le début de l'instruction comme un entier codant la future fonction à exécuter. Du coup, on aurait pu coder avec parseInt().
+      String chaineArguments = INSTRUCTION.substring(posFin);
+#ifdef AFFICHAGE
+      Serial.println("   fonction n° : " + String(numFonction));
+      Serial.println("   chaine d'arguments : " + chaineArguments);
+#endif
+      
+      // récupération des arguments
+      int i=0; int numArg=0;
+      for (i=0;i<chaineArguments.length();i++){ // on compte les arguments
+        if (chaineArguments[i] == separator){numArg++;}
+      }
+#ifdef AFFICHAGE
+      Serial.print("   Il y a "); Serial.print(numArg); Serial.println(" arguments.");
+#endif
+      String arguments[numArg]; // le tableau des arguments
+      for(i=0;i<numArg;i++){
+        chaineArguments = chaineArguments.substring(1); // on enlève le séparateur
+        posFin = chaineArguments.indexOf(separator);  // on repère la fin de l'argument
+        arguments[i] = chaineArguments.substring(0, posFin); // on extrait l'argument
+        chaineArguments = chaineArguments.substring(posFin); // on enlève ce qui a été lu
+#ifdef AFFICHAGE
+        Serial.println("   argument : " + arguments[i]);
+#endif
+      }
+      
+      switch(numFonction){
+        case 1:
+          Serial.println("Youpi !"); break;
         case 2:
-            noel(/*ARG*/);
+          noel(); break;
+        case 3:
+          if(numArg != 0){serialPush(arguments[0]);} break;
+        case 4:
+          if(numArg != 0){Serial.println(arguments[0]);} break;
+        case 5:
+          if(numArg != 0){
+            String message = "";
+            for (i=0;i<numArg;i++){
+              message += arguments[i];
+              message += " ";
+            }
+            Serial.println(message);
+          } break;
         default: // Case of unmatched function. 
-            return; // Panic. 
+#ifdef AFFICHAGE
+            Serial.println("Ca serait gentil de me donner des instructions que je comprends !");
+#endif
+            return; // Panic.
+      }
     }
 }
