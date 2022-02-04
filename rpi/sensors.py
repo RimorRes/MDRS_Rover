f0a323818d3ca90b1e12685b929718e7d8bcf7c9
Maxime Djmb <maxime.djomby@gmail.com>
2022-02-04 17:01:50 +0100

    Update sensors.py

--- rpi/sensors.py
+++ rpi/sensors.py
@@ -1,40 +1,14 @@
-import numpy as np
-from scipy.spatial.transform import Rotation
-from time import sleep
 from sense_hat import SenseHat
 from gpiozero import CPUTemperature
 
 
-g = 9.80665
 sense = SenseHat()
 
 
-def calc_acceleration_vector():
-    raw_accel = list(sense.get_accelerometer_raw().values())
-    pitch, roll, yaw = list(sense.get_gyroscope().values())
-    r = Rotation.from_euler('ZYX', [yaw, pitch, roll], degrees=True)
-    vec = r.apply(np.array(raw_accel))
-
-    return vec
-
-
-def calibrate_imu():
-    n = 10
-    t = 0.01
-
-    s = np.zeros(3)
-    for _ in range(n):
-        s += calc_acceleration_vector()
-        sleep(t)
-    correction = s/n
-
-    return correction
-
-
-def get_imu():
-    accel = calc_acceleration_vector()
-    gyro = list(sense.get_gyroscope().values())
-    compass = sense.get_compass()
+def get_raw_imu():
+    accel = sense.get_accelerometer_raw()
+    gyro = sense.get_gyroscope_raw()
+    compass = sense.get_compass_raw()
 
     return accel, gyro, compass
 
