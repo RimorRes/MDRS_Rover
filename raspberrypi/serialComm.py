22f41805d7f4fe5e068cebf494964a0c07d497ff
Nihoncraft <78471570+Nihoncraft@users.noreply.github.com>
2022-02-04 18:47:15 +0100

    Update serialComm.py

--- raspberrypi/serialComm.py
+++ raspberrypi/serialComm.py
@@ -2,33 +2,15 @@
 # LIST OF AVAILABLE PORTS: python -m serial.tools.list_ports
 
 import serial
+import time
 
 port= serial.Serial('dev/ttyUSB0', 9600, timeout=1000) # modifiable
 
-def serialPush(msg): # txt must be: function_argument; NO GRAMMAR VERIFICATION. 
-  serial.write(msg)
-
-def serialPull(None):
-  return read_until(expected=';', size=None)
-
-def Run(instruction): 
-  if instruction != "": 
-    separator='_'
-    posFin=instruction.index(separator)
-    print("       instruction: "+instruction+". ")
-    print("       taille: "+str(len(instruction))+". ")
-    function=''
-    for i in range(posFin): 
-      function[i]=instruction[i]
-    print("       fonction: "+function+". ")
-    argument=''
-    for i in range(posFin-len(function): 
-      argument[i]=instruction[i]
-    argNum=0
-    for i in range(len(argument)): 
-      if argument[i]==separator: 
-        numArg+=1
-    print("       nombres d'arguments: "+numArg+". ")
-    print("       arguments: "+argument+". ")
-    match function: 
-      case '': 
+def lectureBus():
+  dureeAttente = 1000
+  TIMEOUT = 1000
+  reception = serialPull()
+  if reception == "A": 
+    port.write("R")
+    time.sleep(dureeAttente)
+    
