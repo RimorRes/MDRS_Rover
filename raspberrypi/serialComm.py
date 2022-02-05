7b119d34cb1ede0c66dd8813b98a33ed8f21bfa9
Nihoncraft <78471570+Nihoncraft@users.noreply.github.com>
2022-02-06 00:06:54 +0100

    Modification (Je n'ai pas encore vérifier le bon fonctionnement).

--- raspberrypi/serialComm.py
+++ raspberrypi/serialComm.py
@@ -32,4 +32,52 @@ def parlerBus():
 
 def Run(instruction):
   if instruction != "": 
-    print("instruction: "+instruction)
+    print("instruction: "+instruction+". ")
+    print("taille de l'instruction: "+str(len(instruction))+". ")
+    separator = '_'
+    posFin = instruction.index(separator)
+    numFonction = int(substr(0, posFin))
+    chaîneArguments = instruction.substr(posFin)
+    print("fonction n: "+str(numFonction)+". ")
+    print("chaîne d'argument: "+chaîneArguments+". ")
+    numArg = 0
+    for i in range(len(chaîneArguments)): 
+      if chaîneArguments[i] == separator: 
+        numArg+=1
+    print("Il y a "+numArg+" arguments. ")
+    string argument[numArg]
+    for i in range(numArg): 
+      chaîneArguments = substr(chaîneArguments)
+      posFin = chaîneArguments.index(separator)
+      argument[i] = chaîneArguments.substr(0, posFin)
+      chaîneArguments = chaîneArguments.substr(posFin)
+      print("argument: "+argument[i]+". ")
+
+    match numFonction:
+      case 1: 
+        print("Youpi !")
+      case 2: 
+
+def serialPull(): 
+  string phrase
+  terminator = ';'
+  for i in range(200): 
+    phrase[i] = port.read()
+    if phrase[i]==terminator: 
+      break
+  posFin = phrase.index(char(10))
+  if posFin != -1: 
+    phrase.remove(posFin)
+  return phrase
+
+def serialPush(string txt): 
+  print("Coucou, nous sommes dans serialPush(). ")
+  longueur = len(txt)
+  if longueur != 0: 
+    message[longueur+1]
+    txt=list(txt)
+    print("Texte à transmettre: "+txt+" de longueur "+str(longueur)+". ")
+    print("Texte envoyé: "+str(message))
+    port.write(message, longueur)
+    print("     <-- C'est fait. ")
+  return
