e5a98d126dbc18c0c2b8022669c7aa948ff97a17
Rodolphe Hoppeler <rodolphe.hoppeler@crans.org>
2022-10-10 13:45:11 +0200

    du code en réserve pour les évolutions

---
+++ mission 2023/réserve/receptionAntenneRF.ino
@@ -0,0 +1,49 @@
+/*
+   ______               _                  _///  _           _                   _
+  /   _  \             (_)                |  __\| |         | |                 (_)
+  |  [_|  |__  ___  ___ _  ___  _ __      | |__ | | ___  ___| |_ _ __ ___  _ __  _  ___  _   _  ___
+  |   ___/ _ \| __|| __| |/ _ \| '_ \_____|  __|| |/ _ \/  _|  _| '__/   \| '_ \| |/   \| | | |/ _ \
+  |  |  | ( ) |__ ||__ | | ( ) | | | |____| |__ | |  __/| (_| |_| | | (_) | | | | | (_) | |_| |  __/
+  \__|   \__,_|___||___|_|\___/|_| [_|    \____/|_|\___|\____\__\_|  \___/|_| |_|_|\__  |\__,_|\___|
+                                                                                      | |
+                                                                                      \_|
+  Fichier:      HelloWorldNRF24L01-Recepteur
+  Description:  Réception d'un message "Hello World" depuis un autre arduino nano, via un NRF24L01
+  Auteur:       Passion-Électronique
+
+  Librairie utilisée : https://github.com/nRF24/RF24
+
+  Créé le 19.03.2021
+*/
+#include <SPI.h>
+#include <RF24.h>
+
+#define pinCE   7             // On associe la broche "CE" du NRF24L01 à la sortie digitale D7 de l'arduino
+#define pinCSN  8             // On associe la broche "CSN" du NRF24L01 à la sortie digitale D8 de l'arduino
+#define tunnel  "PIPE1"       // On définit le "nom de tunnel" (5 caractères) à travers lequel on va recevoir les données de l'émetteur
+
+RF24 radio(pinCE, pinCSN);    // Instanciation du NRF24L01
+
+const byte adresse[6] = tunnel;       // Mise au format "byte array" du nom du tunnel
+char message[32];                     // Avec cette librairie, on est "limité" à 32 caractères par message
+
+void setup() {
+  // Initialisation du port série (pour afficher les infos reçues, sur le "Moniteur Série" de l'IDE Arduino)
+  Serial.begin(9600);
+  Serial.println("Récepteur NRF24L01");
+  Serial.println("");
+
+  // Partie NRF24
+  radio.begin();                      // Initialisation du module NRF24
+  radio.openReadingPipe(0, adresse);  // Ouverture du tunnel en LECTURE, avec le "nom" qu'on lui a donné
+  radio.setPALevel(RF24_PA_MIN);      // Sélection d'un niveau "MINIMAL" pour communiquer (pas besoin d'une forte puissance, pour nos essais)
+  radio.startListening();             // Démarrage de l'écoute du NRF24 (signifiant qu'on va recevoir, et non émettre quoi que ce soit, ici)
+}
+
+void loop() {
+  // On vérifie à chaque boucle si un message est arrivé
+  if (radio.available()) {
+    radio.read(&message, sizeof(message));                        // Si un message vient d'arriver, on le charge dans la variable "message"
+    Serial.print("Message reçu : "); Serial.println(message);     // … et on l'affiche sur le port série !
+  }
+}
