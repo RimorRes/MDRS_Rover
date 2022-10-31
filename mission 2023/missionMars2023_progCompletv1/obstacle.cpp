d124c03ca1ba084f8ae41293dffb292d7f3118e3
Nihoncraft <78471570+Nihoncraft@users.noreply.github.com>
2022-10-31 11:16:16 +0100

    Ajout de is_existing_obstacle() .
    Gestion du chemin manquant.

--- mission 2023/missionMars2023_progCompletv1/obstacle.cpp
+++ mission 2023/missionMars2023_progCompletv1/obstacle.cpp
@@ -12,12 +12,17 @@
 #define OBSTACLE_H
 #endif
 
+/*****************************
+* GESTION DE obstacles_liste *
+*****************************/
+
 Point obstacle_map::obstacle_position_from_rover(Point R, float d, float p)const{
 // ...avec R la position du rover, d la distance à l'obstacle et p la direction du rover. 
     return Point(R.getX() + cos(p) * d, R.getY() + cos(p)*d);
 }
 
 void obstacle_map::add_obstacle(Point P){
+    if(is_existing_obstacle(P, 0.1f)){return;}// (en m)
 // Pour ne pas laisser des cases vides...
     for(int i = obstacles_liste_len; i<obstacles_liste_len; i++){
         if(obstacles_liste[i] == Point(0, 0)){
@@ -35,3 +40,17 @@ void obstacle_map::remove_obstacle(int n){
 // ...avec n l'emplacement de l'obstacle à retirer. 
     obstacles_liste[n] = Point(0, 0);
 }
+
+bool obstacle_map::is_existing_obstacle(Point P, float r)const{
+// ...avec r la précision. 
+    for(int i = obstacles_liste_len; i<=obstacles_liste_len; i++){
+        if((obstacles_liste[i].getX()*obstacles_liste[i].getX())+(obstacles_liste[i].getY()*obstacles_liste[i].getY())>=r*r){
+            return true;
+        }
+    }
+    return false;
+}
+
+/********************
+* GESTION DU CHEMIN *
+********************/
--- mission 2023/missionMars2023_progCompletv1/obstacle.h
+++ mission 2023/missionMars2023_progCompletv1/obstacle.h
@@ -5,7 +5,7 @@ public:
     Point obstacle_position_from_rover(Point R, float d, float p)const;
     void add_obstacle(Point P);
     void remove_obstacle(int n);
-    bool is_existing_obstacle(Point P)const;
+    bool is_existing_obstacle(Point P, float r)const;
 private:
     Point* obstacles_liste;
     int obstacles_liste_len = 0;// nombre d'élement dans obstacles_liste
