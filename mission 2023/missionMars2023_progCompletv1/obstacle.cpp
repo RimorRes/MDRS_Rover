678cf0541acb9798272c8e4dca180c883f7e5e98
Nosiulconc <60153710+Nosiulconc@users.noreply.github.com>
2022-11-26 18:57:50 +0100

    formatation du code

--- mission 2023/missionMars2023_progCompletv1/obstacle.cpp
+++ mission 2023/missionMars2023_progCompletv1/obstacle.cpp
@@ -20,35 +20,35 @@
 
 Point Obstacle::obstaclePositionFromRover(Point R, float d, float p)const{
 // ...avec R la position du rover, d la distance à l'obstacle et p la direction du rover. 
-    return Point(R.getX() + cos(p) * d, R.getY() + sin(p) * d);
+  return Point(R.getX() + cos(p) * d, R.getY() + sin(p) * d);
 }
 
 void Obstacle::addObstacle(Point P, float r){
   if(_obstaclesListeLen < _obstaclesListeSize - 1){
     if(isExistingObstacle(P, r)){ return; } // l'obstacle existe déjà dans ces parages
-	  _obstaclesListe[++_obstaclesListeLen] = P; // ++_obstaclesListeLen incremente la variable avant de l'utiliser comme index
+    _obstaclesListe[++_obstaclesListeLen] = P; // ++_obstaclesListeLen incremente la variable avant de l'utiliser comme index
   }
 }
 
 void Obstacle::removeObstacle(int n){
   if(_obstaclesListeLen > 0 && n <= _obstaclesListeLen){
-	  for(int i = n; i < _obstaclesListeLen; i++){
-		  _obstaclesListe[i] = _obstaclesListe[i + 1];
-		}
+    for(int i = n; i < _obstaclesListeLen; i++){
+      _obstaclesListe[i] = _obstaclesListe[i + 1];
+    }
     _obstaclesListeLen--;
-	}
+  }
 }
 
 bool Obstacle::isExistingObstacle(Point P, float r_ignore)const{
-// ...avec r la précision. 
-	const float r2 = r_ignore * r_ignore;
-	for(int i = 0; i < _obstaclesListeLen; i++){
-		float x = _obstaclesListe[i].getX() - P.getX();
-		float y = _obstaclesListe[i].getY() - P.getY();
-		if(x * x + y * y <= r2){ return true; }
-	}
-	return false;
-	//Optimisation future : array bidimentionnel de pointeur de points. Inutile à ce stade.  
+  // ...avec r la précision. 
+  const float r2 = r_ignore * r_ignore;
+  for(int i = 0; i < _obstaclesListeLen; i++){
+    float x = _obstaclesListe[i].getX() - P.getX();
+    float y = _obstaclesListe[i].getY() - P.getY();
+    if(x * x + y * y <= r2){ return true; }
+  }
+  return false;
+  //Optimisation future : array bidimentionnel de pointeur de points. Inutile à ce stade.  
 }
 
 /********************
@@ -57,5 +57,5 @@ bool Obstacle::isExistingObstacle(Point P, float r_ignore)const{
 
 Point Obstacle::cheminCorrection(Point P, float r, float p)const{
 // ...avec P l'obstacle et r la distance entre l'obstacle et le nouveau Point.
-    return Point(P.getX() - sin(p) * r, P.getY() + cos(p) * r);
+  return Point(P.getX() - sin(p) * r, P.getY() + cos(p) * r);
 }
