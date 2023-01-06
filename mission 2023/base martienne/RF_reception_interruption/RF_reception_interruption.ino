b0dbcdb9e7a2cc8e678ce8cf9572a2bf3896dffa
Nosiulconc <nosiulconc@gmail.com>
2023-01-06 19:47:59 +0100

    implementation du protocole de communication

--- mission 2023/base martienne/RF_reception_interruption/RF_reception_interruption.ino
+++ mission 2023/base martienne/RF_reception_interruption/RF_reception_interruption.ino
@@ -1,43 +1,78 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+enum states{ WAITING_FOR_MSG, RECEIVING_PACKETS };
+
 const int PIN_CE = 7;//9;
 const int PIN_CSN = 8;//10;
-char gotByte[32];
-volatile int count = 0;
-int pCount = 0;
-RF24 wirelessSPI(PIN_CE, PIN_CSN);
-//const uint64_t pAdress = 0xB00B1E5000LL; // LL indicates "long long type"
-const byte pAdress[6] = "PIPE2";
+const byte adresses[][6] = {"PIPE1", "PIPE2"};
+const char msgConfirmation[] = "R";
+
+char radioState = WAITING_FOR_MSG;
+boolean msgReceived = false;
+
+char msgEntrant[32];
+String msg;
+
+RF24 radio(PIN_CE, PIN_CSN);
 
 void setup() {
-  Serial.begin(9600);  wirelessSPI.begin();
+  Serial.begin(9600);
+  radio.begin();
   
-  wirelessSPI.setAutoAck(1);
-//  wirelessSPI.enableAckPayload();
-  wirelessSPI.maskIRQ(1, 1, 0);
-  wirelessSPI.setPALevel(RF24_PA_MIN);//LOW);
-  wirelessSPI.openReadingPipe(1, pAdress);
-  wirelessSPI.startListening();
-  attachInterrupt(1, interruptFunction, FALLING);
-  Serial.println("coucou setup");
+  radio.setAutoAck(1);
+//  radio.enableAckPayload();
+  radio.maskIRQ(1, 1, 0);
+  radio.setPALevel(RF24_PA_MIN);//LOW);
+  radio.openWritingPipe(adresses[0]);
+  radio.openReadingPipe(1, adresses[1]);
+  radio.startListening();
+  
+  attachInterrupt(1, dataReceivedInterrupt, FALLING);
+  
+  Serial.println("Setup terminé.");
 }
 
-void loop() {
-  Serial.println("coucou loop, count = " + String(count));
-  if(pCount>count){
-//    Serial.begin(9600); // 57600
-    Serial.println("coucou loop");
-    Serial.print("Received pacet number ");
-    Serial.println(count);
-//    Serial.end();
-    pCount = count;
+void loop(){
+  if(msgReceived){
+    msgReceived = false;
+
+    if(radioState == WAITING_FOR_MSG){
+      if(msgEntrant[0] == 'A'){
+        radioState = RECEIVING_PACKETS;
+        msg = "";
+        confirmReception();
+      }
+      else if(msgEntrant[0] == 'F'){ // J'ai mis 'F' mais je ne sais pas comment le rover signale la fin d'un message
+        confirmReception();
+      }
+    }
+    else{ //radioState == RECEIVING_PACKETS
+      if(msgEntrant[0] == 'A'){
+        confirmReception();
+      }
+      else if(msgEntrant[0] == 'F'){ // Pareil ici
+        radioState = WAITING_FOR_MSG;
+        Serial.println("Message reçu: " + msg);
+        confirmReception();
+      }
+      else{
+        msg += msgEntrant;
+      }
+    }
   }
 }
 
-void interruptFunction(){
-  count++;
-  while(wirelessSPI.available()){
-    wirelessSPI.read(&gotByte, sizeof(gotByte));
+void confirmReception(){
+  radio.stopListening();
+  radio.write(msgConfirmation, sizeof(msgConfirmation));
+  delay(5); // Delai à determiner plus précisement
+  radio.startListening();
+}
+
+void dataReceivedInterrupt(){
+  if(radio.available()){
+    radio.read(msgEntrant, sizeof(msgEntrant));
+    msgReceived = true;
   }
 }
